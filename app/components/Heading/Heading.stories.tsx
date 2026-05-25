287d0ffeef46816d48a3acb29abb4a473195bdea
Cyronp <vitorhenriquedasilveira@gmail.com>
2026-05-24 21:13:26 -0300

    feat: add Storybook for testing components, add Heading storybook

---
+++ app/components/Heading/Heading.stories.tsx
@@ -0,0 +1,56 @@
+import Heading from "./Heading";
+import type { Meta, StoryObj } from "@storybook/react";
+import type { HeadingProps } from "./HeadingProps";
+
+const meta: Meta<typeof Heading> = {
+  title: "Typography/Heading",
+  component: Heading,
+  argTypes: {
+    as: { control: "text", description: "Tag HTML do component" },
+    asChild: { control: "boolean", description: "Renderizar asChild" },
+    variant: {
+      control: { type: "select" },
+      options: ["Primary", "Secondary", "Terciary"],
+      description: "Variantes do cabeçalho",
+    },
+    className: { control: "text", description: "TailwindCSS customizados" },
+    children: { control: "text", description: "Contéudo do cabeçalho" },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: "Cabecalho flexivel de HTML",
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Heading>;
+
+export const Default: Story = {
+  args: {
+    children: "Primário Heading",
+    variant: "Primary",
+    as: "h1",
+    asChild: false,
+    className: "",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    children: "Secundário Heading",
+    variant: "Secondary",
+    as: "h3",
+  },
+};
+
+export const Terciary: Story = {
+  args: {
+    children: "Terciário Heading",
+    variant: "Terciary",
+    as: "h4",
+  },
+};
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen max-w-360 mx-auto items-center">
       <Header />
-      <main className="flex-grow w-full flex flex-col items-center">
+      <main className="grow w-full flex flex-col items-center">
         <CTASection />
         <ContentSection />
       </main>
